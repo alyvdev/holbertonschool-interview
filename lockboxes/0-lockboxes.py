527a84084113a089017b0b4fe2fccd86328943f7
Ali Aliyev <alyvofficial@gmail.com>
2024-10-31 11:56:41 +0400

    add test cases for canUnlockAll function in 0-lockboxes.py

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -13,3 +13,13 @@ def canUnlockAll(boxes):
                 to_check.append(key)
                 
     return len(opened_boxes) == len(boxes)
+
+if __name__ == "__main__":
+    boxes1 = [[1], [2], [3], [4], []]
+    print(canUnlockAll(boxes1))
+
+    boxes2 = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+    print(canUnlockAll(boxes2))
+
+    boxes3 = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+    print(canUnlockAll(boxes3))
