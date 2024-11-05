21f3989f11f5c66d41caf07e93d3a4a7a20114b8
Ali Aliyev <alyvofficial@gmail.com>
2024-11-05 11:28:29 +0400

    feat: implement insert_node function for inserting a number in a sorted linked list; add build tasks for C/C++ in VSCode

--- insert_in_sorted_linked_list/0-insert_number.c
+++ insert_in_sorted_linked_list/0-insert_number.c
@@ -7,7 +7,7 @@
  * @number: number to insert.
  * Return: address of the new node or NULL if it failed.
  */
-listint_t *insert_node(listint_t **head, int number);
+listint_t *insert_node(listint_t **head, int number)
 {
     listint_t *new;
     listint_t *current;
