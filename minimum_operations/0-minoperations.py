8a8a7a1ce0b67c80b8dcb7c16d47e9a277b7ea2f
Ali Aliyev <alyvofficial@gmail.com>
2024-11-24 22:18:35 +0400

    feat: implement function to calculate minimum operations for a given number

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,13 @@
+def minOperations(n):
+    if n <= 1:
+        return 0
+    operations = 0
+    divions = 2
+
+    while n > 1:
+        while n % divions == 0:
+            operations += divions
+            n = n / divions
+        divions += 1
+    
+    return operations
