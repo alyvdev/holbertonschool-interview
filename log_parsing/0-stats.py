1941133e454707e8a1671dbb49ad8a8e8ab02f81
Ali Aliyev <alyvofficial@gmail.com>
2024-12-15 13:52:31 +0400

    fix: improve docstring formatting in log_parsing/0-stats.py

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" Log parsing """
+"""Log parsing """
 
 import sys
 
@@ -9,6 +9,7 @@ status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
 line_count = 0
 
 def print_stats():
+    """ Prints the log stats"""
     print("File size: {}".format(total_size))
     for code in sorted(status_codes.keys()):
         if status_codes[code] > 0:
