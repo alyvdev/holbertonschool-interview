21cd638b3ea4f53de68c7564326bb188dfe4b3cd
Ali Aliyev <alyvofficial@gmail.com>
2025-01-26 21:55:11 +0400

    Fixed

--- slide_line/slide_line.h
+++ slide_line/slide_line.h
@@ -1,11 +1,14 @@
 #ifndef SLIDE_LINE_H
 #define SLIDE_LINE_H
 
-#include <stddef.h>
-
-#define SLIDE_LEFT 0
+#define SLIDE_LEFT 2
 #define SLIDE_RIGHT 1
 
+#include <stdio.h>
+#include <stdlib.h>
+
 int slide_line(int *line, size_t size, int direction);
+int to_left(int *line, size_t size);
+int to_right(int *line, size_t size);
 
 #endif /* SLIDE_LINE_H */
